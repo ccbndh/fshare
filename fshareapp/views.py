5627a6ed1f746505803feeba861ff119c02fafdc
Tin <tin.nguyen@alphabase.vn>
2015-06-24 23:24:35 +0700

    add script getlink fshare to django

--- fshareapp/views.py
+++ fshareapp/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.fshare.vn/login/'
+username = 'share@linksvip.net'
+password = '18065226f'
 
 
 def index(request):
+
+    session = requests.Session()
+
+    # getting csrf value
+
+    response = session.get(url)
+    soup = BeautifulSoup(response.content)
+
+    form = soup.form
+    csrf = form.find('input', attrs={'name': 'fs_csrf'}).get('value')
+
+    # logging in
+    data = {
+        'LoginForm[email]': username,
+        'LoginForm[password]': password,
+        'fs_csrf': csrf
+    }
+
+    headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.10 Safari/537.36',
+    'referer': url}
+
+
+
+    res = ''
+    try:
+        response = session.post(url, data=data, headers=headers)
+        response = session.get(request.POST['url'], allow_redirects=False, timeout=(1.0, 10.0))
+        res = response.headers['location']
+    except:
+        res = "Can't get link"
+
+
     return render(request, "index.html", {
-        'temp': 'temp',
+        'res': res,
     })
